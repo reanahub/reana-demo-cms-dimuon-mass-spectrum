a592d05a660d4bf2f5a0dad8ca6a996f48910812
Radovan Lascsak <radovan.lascsak@gmail.com>
2019-09-26 13:39:36 +0200

    workflow: standalone run.sh script to run analysis in docker
    * (closes #2)
    
    Signed-off-by: Radovan Lascsak <radovan.lascsak@gmail.com>

---
+++ run.sh
@@ -0,0 +1,24 @@
+#/bin/zsh
+
+# This script uses standalone Docker. Run it as follows:
+#
+# $ docker run --rm -i -t -v `pwd`:/w  cmsopendata/cmssw_5_3_32 /w/run.sh
+#
+# Check results via:
+#
+# $ root results.root
+
+
+source /opt/cms/cmsset_default.sh
+scramv1 project CMSSW CMSSW_5_3_32
+cd CMSSW_5_3_32/src
+eval `scramv1 runtime -sh`
+cd ../..
+mkdir reana-demo-cms-dimuon-mass-spectrum
+cd reana-demo-cms-dimuon-mass-spectrum
+mkdir DimuonSpectrum2011
+cd DimuonSpectrum2011
+cp -a /w/* .
+scram b
+cmsRun ./demoanalyzer_cfg.py
+cp DoubleMu.root /w/results.root
