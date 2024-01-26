d0d511540c4c8fcae8ea40b0831d7c31a2823f75
Tibor Šimko <tibor.simko@cern.ch>
2024-01-26 10:09:32 +0100

    ci(shellcheck): fix exit code propagation (#15)
    Return properly the exit code status of shellcheck command when looping
    through shell script files.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#/bin/zsh
+#!/usr/bin/env bash
 
 # This script uses standalone Docker. Run it as follows:
 #
@@ -8,16 +8,16 @@
 #
 # $ root results.root
 
-
+# shellcheck disable=SC1091
 source /opt/cms/cmsset_default.sh
 scramv1 project CMSSW CMSSW_5_3_32
-cd CMSSW_5_3_32/src
-eval `scramv1 runtime -sh`
+cd CMSSW_5_3_32/src || exit
+eval "$(scramv1 runtime -sh)"
 cd ../..
 mkdir reana-demo-cms-dimuon-mass-spectrum
-cd reana-demo-cms-dimuon-mass-spectrum
+cd reana-demo-cms-dimuon-mass-spectrum || exit
 mkdir DimuonSpectrum2011
-cd DimuonSpectrum2011
+cd DimuonSpectrum2011 || exit
 cp -a /w/* .
 scram b
 cmsRun ./demoanalyzer_cfg.py
